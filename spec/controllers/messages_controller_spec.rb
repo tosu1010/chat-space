a05e0333ebc605bc0845e519c0cbbd7e9a1813ab
tosu1010 <ikiblea10@gmail.com>
2019-08-03 12:32:31 +0900

    message#create_test

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -35,4 +35,58 @@ RSpec.describe MessagesController do
       end
     end
   end
+
+  describe "POST #create" do
+    let(:params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message) } }
+
+    context "logged in" do
+      before do
+        # controller_macros.rbで定義したもの
+        login user
+      end
+
+      context "can save" do
+        subject {
+          post :create,
+          params: params
+        }
+
+        it "count up message" do
+          expect{ subject }.to change(Message, :count).by(1)
+        end
+
+        it "redirects to group_messages_path" do
+          subject
+          expect(response).to redirect_to(group_messages_path(group))
+        end
+      end
+
+      context "can't save" do
+        let(:invalid_params) { { group_id: group.id, user_id: user.id, message: attributes_for(:message, body: nil, image: nil) } }
+
+        subject {
+          post :create,
+          params: invalid_params
+        }
+
+        it "dose not count up" do
+          subject
+          expect{ subject }.not_to change(Message, :count)
+        end
+
+        it "renders index" do
+          subject
+          expect(response).to render_template :index
+        end
+      end
+    end
+
+    context "not logged in" do
+
+      it "redirects to new_user_session_path" do
+        post :create, params: params
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+  end
 end
