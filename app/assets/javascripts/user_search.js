13149b16feaea247f4e2750eae1328399630e923
tosu1010 <ikiblea10@gmail.com>
2019-08-11 12:13:56 +0900

    add_user_to_new_chat_member
    追加ボタンを押したユーザーをチャットメンバーに追加する。

--- app/assets/javascripts/user_search.js
+++ app/assets/javascripts/user_search.js
@@ -6,6 +6,16 @@ $(function(){
                 </div>`
     return html;
   };
+
+  function add_user_html(user_id, user_name){
+    let html = `<div class='chat-group-user'>
+                  <input name='group[user_ids][]' type='hidden' value=${user_id}>
+                  <p class='chat-group-user__name'>${user_name}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    return html;
+  };
+
   $("#user-search-field").on("keyup", function(e){
     e.preventDefault();
     let input_name = $(this).val();
@@ -18,7 +28,7 @@ $(function(){
       dataType: "json"
     })
     .done(function(users){
-      $(".chat-group-user").remove();
+      $(".chat-group-user.clearfix").remove();
       if(input_name.length !== 0){
         $.each(users, function(i, user){
           let html = buildHTML(user)
@@ -29,5 +39,13 @@ $(function(){
     .fail(function(){
       alert("ユーザー検索に失敗しました");
     })
+  });
+
+  $("#user-search-result").on("click", ".user-search-add", function(){
+    let user_id = $(this).attr("data-user-id")
+    let user_name = $(this).attr("data-user-name")
+    let html = add_user_html(user_id, user_name);
+    $(this).parent().remove();
+    $("#chat-member-list").append(html);
   });
 });
