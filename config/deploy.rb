d247f56933b82dbd9af54a0341ecc4fe426ee5dd
tosu1010 <ikiblea10@gmail.com>
2019-08-15 15:36:55 +0900

    fix_deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "3.11.0"
 
 # Capistranoのログの表示に利用する
 set :application, 'chat-space'
@@ -15,7 +15,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
  
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/DaitoSuzuki/Documents/.ssh/tech-exp-key.pem']
+                  keys: ['~/Documents/.ssh/tech-exp-key.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
